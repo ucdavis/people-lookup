421896fa0533f618c84a73fe093b938d3aac2342
Jason Sylvestre <jsylvestre@ucdavis.edu>
2019-04-24 14:47:10 -0700

    Cas

--- src/PeopleLookup.Mvc/Controllers/SuperController.cs
+++ src/PeopleLookup.Mvc/Controllers/SuperController.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace PeopleLookup.Mvc.Controllers
 {
     [AutoValidateAntiforgeryToken]
-    //[Authorize]
+    [Authorize]
     public class SuperController : Controller
     {
         private const string TempDataMessageKey = "Message";
--- src/PeopleLookup.Mvc/Services/IdentityService.cs
+++ src/PeopleLookup.Mvc/Services/IdentityService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Ietws;
@@ -11,6 +12,7 @@ namespace PeopleLookup.Mvc.Services
     public interface IIdentityService
     {
         Task<SearchResult> Lookup(string search);
+        Task<User> GetByKerberos(string kerb);
     }
 
     public class IdentityService : IIdentityService
@@ -40,6 +42,71 @@ namespace PeopleLookup.Mvc.Services
             }
 
             return searchResult;
+        }
+
+        public async Task<User> GetByKerberos(string kerb)
+        {
+            var clientws = new IetClient(_authSettings.IamKey);
+            var ucdKerbResult = await clientws.Kerberos.Search(KerberosSearchField.userId, kerb);
+
+            if (ucdKerbResult.ResponseData.Results.Length == 0)
+            {
+                return null;
+            }
+
+            if (ucdKerbResult.ResponseData.Results.Length != 1)
+            {
+                var iamIds = ucdKerbResult.ResponseData.Results.Select(a => a.IamId).Distinct().ToArray();
+                var userIDs = ucdKerbResult.ResponseData.Results.Select(a => a.UserId).Distinct().ToArray();
+                if (iamIds.Length != 1 && userIDs.Length != 1)
+                {
+                    throw new Exception($"IAM issue with non unique values for kerbs: {string.Join(',', userIDs)} IAM: {string.Join(',', iamIds)}");
+                }
+            }
+
+            var ucdKerbPerson = ucdKerbResult.ResponseData.Results.First();
+
+            // find their email
+            var ucdContactResult = await clientws.Contacts.Get(ucdKerbPerson.IamId);
+
+            if(ucdContactResult.ResponseData.Results.Length == 0)
+            {
+                return null;
+            }
+
+            var ucdContact = ucdContactResult.ResponseData.Results.First();
+            var rtValue = CreateUser(ucdContact.Email, ucdKerbPerson, ucdKerbPerson.IamId);
+
+            if (string.IsNullOrWhiteSpace(rtValue.Email))
+            {
+                if (!string.IsNullOrWhiteSpace(ucdKerbPerson.UserId))
+                {
+                    rtValue.Email = $"{ucdKerbPerson.UserId}@ucdavis.edu";
+                }
+            }
+
+            var results = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(rtValue, new ValidationContext(rtValue), results);
+            if (!isValid)
+            {
+                return null;
+            }
+
+
+            return rtValue;
+        }
+
+                private User CreateUser(string email, KerberosResult ucdKerbPerson, string iamId)
+        {
+            var user = new User()
+            {
+                FirstName = ucdKerbPerson.FirstName,
+                LastName = ucdKerbPerson.LastName,
+                Id = ucdKerbPerson.UserId,
+                Email = email,
+                Iam = iamId
+            };
+            return user;
         }
 
         private async void LookupAssociations(string iamId, SearchResult searchResult)

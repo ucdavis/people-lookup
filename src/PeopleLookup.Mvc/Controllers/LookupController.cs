fbbea5cf605111302964be1aed8925edbee0b823
Jason Sylvestre <jsylvestre@ucdavis.edu>
2019-04-24 08:20:18 -0700

    Creating Lookup

---
+++ src/PeopleLookup.Mvc/Controllers/LookupController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PeopleLookup.Mvc.Models;
+
+namespace PeopleLookup.Mvc.Controllers
+{
+    public class LookupController : Controller
+    {
+        public IActionResult Bulk()
+        {
+            var model = new BulkModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public Task<ActionResult> Bulk(BulkModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/PeopleLookup.Mvc/Models/BulkModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleLookup.Mvc.Models
+{
+    public class BulkModel
+    {
+        [DataType(DataType.Text)]
+        public string BulkEmail { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string BulkKerb { get; set; }
+    }
+}
